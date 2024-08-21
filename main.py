561c9a011b097be6b29405b2fb867bc1962d6cac
Simonwristel <Simon.wristel@elev.ga.ntig.se>
2024-08-21 11:14:38 +0200

    Problemlösning

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # Dice sum probability calculator
 # Författare: 
 # Datum:
+import os
+os.system('cls')
 
 def main():
-    user_input = input().split(" ")
+    user_input = [int(x) for x in input("Skriv in två siffror mellan 4 och 20 ").split(" ")]
+    min_dice = min(user_input)
+    max_dice = max(user_input)
 
-if __name__ == "__main__":
+    for sum in range(min_dice + 1, max_dice + 2):
+        print(sum)
+
+if __name__ == "__main__": 
     main()
